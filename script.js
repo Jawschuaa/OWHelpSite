b46c33a4ca6b5d87a53d3669319c17a2732050d5
Joshua Anderson <myasscantpass@gmail.com>
2023-07-11 20:29:39 -0500

    ignore

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ function toggleMenu() {
     menuItems.classList.add("showing");
   }
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const homeSection = document.querySelector("#Home");
+
+  // Add event listener for scroll to toggle zoom-out class
+  window.addEventListener("scroll", function () {
+    if (isElementInViewport(homeSection)) {
+      homeSection.classList.add("zoom-out");
+    } else {
+      homeSection.classList.remove("zoom-out");
+    }
+  });
+
+  // Check if an element is in the viewport
+  function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+});
